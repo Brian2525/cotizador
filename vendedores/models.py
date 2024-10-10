10d734ca484b98b42df6c5e2e026bd637d4298d1
Brian <juarez.brian1@gmail.com>
2024-10-09 18:01:15 -0600

    Se ha creado la app de sitio web y se ha asociado a cada vendedor un usuario para  en la app de vendedores para las business card

--- vendedores/models.py
+++ vendedores/models.py
@@ -1,7 +1,9 @@
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class Vendedor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     nombre = models.CharField(max_length=100)
     correo = models.EmailField()
     telefono = models.CharField(max_length=15)
